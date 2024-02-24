e76553a9e02efd1ed21ae21e36c016e858c8a03e
MFabri31 <medinajose3195@gmail.com>
2024-02-24 20:54:20 -0300

    add bg image to  the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import Logo from "../assets/icons/icon-dictionary-2.png";
 
 const Header = () => {
   return (
-    <header className="d-flex align-items-center justify-content-center py-5 curved">
+    <header className="d-flex align-items-center justify-content-center py-5 curved header">
       <img src={Logo} alt="icon dictionary" width="60px" />
-      <h1 className="display-4 m-0">English Dictionary</h1>
+      <h1 className="display-3 m-0">English Dictionary</h1>
     </header>
   );
 };

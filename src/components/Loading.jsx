41e4785a1158c11d3bb1d4c80a8b6c2dbcbd74c6
MFabri31 <medinajose3195@gmail.com>
2024-02-25 02:34:14 -0300

    change the spinner variant

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,10 +3,7 @@ import { Spinner } from "react-bootstrap";
 const Loading = () => {
   return (
     <div className="text-center my-5">
-      <Spinner
-        animation="border"
-        role="status"
-      >
+      <Spinner animation="border" role="status" variant="primary">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
     </div>

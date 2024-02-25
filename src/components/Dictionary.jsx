fc3511239cd21f3eefd87aa63f0f65599efe574e
MFabri31 <medinajose3195@gmail.com>
2024-02-25 03:04:25 -0300

    change the font size of the meanings

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -9,19 +9,27 @@ const Dictionary = ({ wordData, loading, error }) => {
 
   const definitionsOfWord = wordData.meanings[0]?.definitions.map(
     (meanings) => {
-      return <li key={meanings.index}>{meanings.definition}</li>;
+      return (
+        <li className="h5" key={meanings.index}>
+          {meanings.definition}
+        </li>
+      );
     }
   );
 
   const definitionsOfVerb = wordData.meanings[1]?.definitions.map(
     (meanings) => {
-      return <li key={meanings.index}>{meanings.definition}</li>;
+      return (
+        <li className="h5" key={meanings.index}>
+          {meanings.definition}
+        </li>
+      );
     }
   );
 
   return (
     <div className="text-start">
-      <p className="fw-bold display-6 fst-italic text-capitalize">
+      <p className="fw-bold display-5 fst-italic text-capitalize">
         <span className="fw-bold">{wordData.word}</span>
       </p>
       <NounDefinitions definitionsOfWord={definitionsOfWord} />
